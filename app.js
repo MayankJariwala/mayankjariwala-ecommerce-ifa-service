6c733189815c7b1a3a565f9c8c95f34815f74f3b
Mayank Jariwala <mayank.jariwala@mindgeeek.com>
2021-11-27 19:57:53 -0500

    .production file added

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors()); // Use this after the variable declaration
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors()); // Use this after the variable declaration
 
 // app.use("/", indexRouter);
 app.use("/v1/auth", auth_router);
